b7214fe45bb65075d9debcfa436b725cf33f9dfd
Muhammad Mahad Tariq <mahad.tariq@emumba.com>
2020-05-01 15:06:05 +0500

    Remove unnecessary jsx code and inline styling from logTable file

--- src/components/logTable.jsx
+++ src/components/logTable.jsx
@@ -13,43 +13,27 @@ function LogTable(props) {
       hours,
     } = time(item.millis);
 
-    if (item.action === "Pause") {
-      return (
-        <React.Fragment>
-          <div style={{ color: "gray" }}>{`#${index + 1}`}</div>
-          <div style={{ color: `rgb(251, 101, 127)` }}>
-            {("00" + hours).slice(-2) +
-              ":" +
-              ("00" + minutes).slice(-2) +
-              ":" +
-              ("00" + seconds).slice(-2) +
-              "." +
-              hundredthMillis +
-              ("" + tenthMillis) +
-              ("" + unitMillis)}
-          </div>
-          <div style={{ color: `rgb(173, 173, 173)` }}>{"Pause"}</div>
-        </React.Fragment>
-      );
-    } else if (item.action === "Split") {
-      return (
-        <React.Fragment>
-          <div style={{ color: "gray" }}>{`#${index + 1}`}</div>
-          <div style={{ color: `rgb(242, 158, 38)` }}>
-            {("00" + hours).slice(-2) +
-              ":" +
-              ("00" + minutes).slice(-2) +
-              ":" +
-              ("00" + seconds).slice(-2) +
-              "." +
-              hundredthMillis +
-              ("" + tenthMillis) +
-              ("" + unitMillis)}
-          </div>
-          <div style={{ color: `rgb(173, 173, 173)` }}>{"Split"}</div>
-        </React.Fragment>
-      );
-    }
+    return (
+      <React.Fragment>
+        <div className="gray">{`#${index + 1}`}</div>
+        <div
+          className={
+            item.action === "Pause" ? "pauseLogEntry" : "splitLogEntry"
+          }
+        >
+          {("00" + hours).slice(-2) +
+            ":" +
+            ("00" + minutes).slice(-2) +
+            ":" +
+            ("00" + seconds).slice(-2) +
+            "." +
+            hundredthMillis +
+            ("" + tenthMillis) +
+            ("" + unitMillis)}
+        </div>
+        <div className="lightGray">{item.action}</div>
+      </React.Fragment>
+    );
   });
   return <div id="container">{markup}</div>;
 }
