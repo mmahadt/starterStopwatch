7037b1f5582ad30a1f066557c9126b5e2f82371c
Muhammad Mahad Tariq <mahad.tariq@emumba.com>
2020-05-01 19:32:00 +0500

    Final code

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,13 +1,9 @@
 import React, { Component } from "react";
 
 function Button(props) {
-  const { className, disabled, buttonName } = props;
+  const { className, disabled, buttonName, handler } = props;
   return (
-    <button
-      className={className}
-      onClick={() => props.handler()}
-      disabled={disabled}
-    >
+    <button className={className} onClick={() => handler()} disabled={disabled}>
       {buttonName}
     </button>
   );
